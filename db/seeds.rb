8e4f877d8318b6e6ef5b2a47013d9b0166eda961
Aaron Harrison <aaronharrison16@gmail.com>
2018-04-08 21:48:58 +0000

    Implemented custom scope for portfolio feature

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
+3.times do |topic|
+  Topic.create!(
+    title: "Topic #{topic}"
+    )
+end
+
+puts "3 topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Nam vitae consequat quam. Nullam tincidunt facilisis tortor et egestas. Nullam non vehicula massa, eget iaculis magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id neque at turpis tincidunt placerat. Quisque a dui euismod, ornare quam sed, accumsan sapien. Vestibulum placerat et ex sit amet finibus. Nulla tincidunt odio eu lectus gravida vehicula. Mauris ullamcorper viverra consectetur. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris sed consectetur tellus. Sed in molestie augue, a scelerisque sem. Phasellus eget nibh justo. Suspendisse potenti. Nam et ipsum in quam egestas porta. Cras convallis dictum sagittis."
+    body: "Nam vitae consequat quam. Nullam tincidunt facilisis tortor et egestas. Nullam non vehicula massa, eget iaculis magna. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id neque at turpis tincidunt placerat. Quisque a dui euismod, ornare quam sed, accumsan sapien. Vestibulum placerat et ex sit amet finibus. Nulla tincidunt odio eu lectus gravida vehicula. Mauris ullamcorper viverra consectetur. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris sed consectetur tellus. Sed in molestie augue, a scelerisque sem. Phasellus eget nibh justo. Suspendisse potenti. Nam et ipsum in quam egestas porta. Cras convallis dictum sagittis.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -16,10 +25,20 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
     subtitle: "Ruby on Rails",
+    body: "Nulla tincidunt odio eu lectus gravida vehicula. Mauris ullamcorper viverra consectetur. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris sed consectetur tellus.",
+    main_image: "http://placehold.it/600x400", 
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
     body: "Nulla tincidunt odio eu lectus gravida vehicula. Mauris ullamcorper viverra consectetur. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris sed consectetur tellus.",
     main_image: "http://placehold.it/600x400", 
     thumb_image: "http://placehold.it/350x200"
