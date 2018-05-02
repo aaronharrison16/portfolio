0e1b32cc5c6f1eb050f2e0f6274d46886d35f22d
Aaron Harrison <aaronharrison16@gmail.com>
2018-05-01 19:53:13 -0600

    Integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    HarrisonViewTool::Renderer.copyright 'Aaron Harrison', 'All rights reserved'
+  end
 end
