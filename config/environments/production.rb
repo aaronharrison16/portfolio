1021b28d64aa666c1dffee93ac16a42c7e13f7dd
Aaron Harrison <aaronharrison16@gmail.com>
2018-05-29 18:24:58 -0600

    integrated production calls for actioncable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://aaronharrison-portfolio.herokuapp.com', 'http://aaronharrison-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://aaronharrison-portfolio.herokuapp.com/cable"
 end
